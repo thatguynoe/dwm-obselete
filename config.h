2fbe3df8a5d68b0f59474e1eb1056083dcebcf8e
thatguynoe <noereyesrivas@gmail.com>
2021-05-17 15:29:20 -0400

    input camtoggle script directly and fix webcam lag

--- config.h
+++ config.h
@@ -182,7 +182,7 @@ static Key keys[] = {
     { MODKEY,                       XK_F8,                      spawn,          SHCMD("mw -Y; pkill -RTMIN+12 dwmblocks") },
     { MODKEY,                       XK_F9,                      spawn,          SHCMD("dmenumount") },
     { MODKEY,                       XK_F10,                     spawn,          SHCMD("dmenuumount") },
-    { MODKEY,                       XK_F11,                     spawn,          SHCMD("camtoggle") },
+    { MODKEY,                       XK_F11,                     spawn,          SHCMD("mpv --no-cache --no-osc --no-input-default-bindings --profile=low-latency --untimed --vf=hflip --no-keepaspect-window --panscan=1 --input-conf=/dev/null --title=webcam $(ls /dev/video[0,2,4,6,8] | tail -n 1)") },
     { MODKEY,                       XK_F12,                     spawn,          SHCMD("refbar") },
 
     { 0,                            XF86XK_Tools,               spawn,          SHCMD(TERMINAL " -e ncmpcpp") },
