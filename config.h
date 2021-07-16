1580c93bd4289cbe977c82eb3ef4c630b0ce1a87
thatguynoe <noereyesrivas@gmail.com>
2021-07-15 23:09:35 -0400

    use floating calculator

--- config.h
+++ config.h
@@ -199,7 +199,7 @@ static Key keys[] = {
     { 0,                            XF86XK_AudioMedia,          spawn,          SHCMD(TERMINAL " -e ncmpcpp") },
     { 0,                            XF86XK_AudioMicMute,        spawn,          SHCMD("pactl set-source-mute @DEFAULT_SOURCE@ toggle") },
     { 0,                            XF86XK_PowerOff,            spawn,          SHCMD("sudo shutdown -h now") },
-    { 0,                            XF86XK_Calculator,          spawn,          SHCMD(TERMINAL " -e bc -l") },
+    { 0,                            XF86XK_Calculator,          togglescratch,  {.ui = 1} },
     { 0,                            XF86XK_Sleep,               spawn,          SHCMD("sudo -A zzz") },
     { 0,                            XF86XK_WWW,                 spawn,          SHCMD("$BROWSER") },
     { 0,                            XF86XK_DOS,                 spawn,          SHCMD(TERMINAL) },
