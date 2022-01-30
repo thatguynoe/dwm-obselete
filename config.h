54d0b62d57a786a72b4c442e35615f5cf3cfa6cf
thatguynoe <noereyesrivas@gmail.com>
2022-01-30 09:59:13 -0500

    don't use tag masks for Firefox

--- config.h
+++ config.h
@@ -48,7 +48,7 @@ static const Rule rules[] = {
      */
     /* class     instance  title                    tags mask   isfloating  isterminal  noswallow  monitor */
     { "Gimp",    NULL,     NULL,                    0,          1,          0,           0,        -1 },
-    { "Firefox", NULL,     NULL,                    1 << 8,     0,          0,          -1,        -1 },
+    { "Firefox", NULL,     NULL,                    0,          0,          0,          -1,        -1 },
     { NULL,      NULL,     "Picture-in-Picture",    0,          1,          0,           0,        -1 },
     { TERMINAL,  NULL,     NULL,                    0,          0,          1,           0,        -1 },
     { NULL,      NULL,     "Event Tester",          0,          0,          0,           1,        -1 }, /* xev */
