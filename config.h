6cc03b1340477390ecae3d3a2ae59bc721e9e486
thatguynoe <noereyesrivas@gmail.com>
2022-02-15 13:04:24 -0500

    set snap pixel to screen gaps distance

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const unsigned int gappih    = 20;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
