00d55b91c0e1326fee0815a0545d6276a79b5c52
thatguynoe <noereyesrivas@gmail.com>
2022-04-18 18:11:07 -0400

    consistent formatting in stackkeys

--- config.h
+++ config.h
@@ -79,9 +79,9 @@ static const Layout layouts[] = {
     { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1 << TAG} }, \
     { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1 << TAG} },
 #define STACKKEYS(MOD,ACTION) \
-    { MOD, XK_j,     ACTION##stack, {.i = INC(+1) } }, \
-    { MOD, XK_k,     ACTION##stack, {.i = INC(-1) } }, \
-    { MOD, XK_v,     ACTION##stack, {.i = 0 } }, \
+    { MOD,                          XK_j,     ACTION##stack,  {.i = INC(+1) } }, \
+    { MOD,                          XK_k,     ACTION##stack,  {.i = INC(-1) } }, \
+    { MOD,                          XK_v,     ACTION##stack,  {.i = 0 } },
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
